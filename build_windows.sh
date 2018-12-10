edb078c36411229dc10b24b9074fbb2a54439e58
Ben Moss <ben@mossity.com>
2018-12-10 11:03:23 -0800

    WIP; update mktemp call

--- build_windows.sh
+++ build_windows.sh
@@ -5,7 +5,7 @@ cd $(dirname "$0")
 ORG_PATH="github.com/containernetworking"
 export REPO_PATH="${ORG_PATH}/plugins"
 
-export GOPATH=$(mktemp -d -t cni)
+export GOPATH=$(mktemp -d)
 mkdir -p ${GOPATH}/src/${ORG_PATH}
 trap "{ rm -rf $GOPATH; }" EXIT
 ln -s ${PWD} ${GOPATH}/src/${REPO_PATH} || exit 255
